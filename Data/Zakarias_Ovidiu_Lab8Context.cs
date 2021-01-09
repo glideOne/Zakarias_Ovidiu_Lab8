08b0f100cc9e8b51fa818fc151607054967d4798
Zakarias Ovidiu <zakarias.ovidiu@gmail.com>
2021-01-09 16:26:13 +0200

    Add project files.

---
+++ Data/Zakarias_Ovidiu_Lab8Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Zakarias_Ovidiu_Lab8.Models;
+
+namespace Zakarias_Ovidiu_Lab8.Data
+{
+    public class Zakarias_Ovidiu_Lab8Context : DbContext
+    {
+        public Zakarias_Ovidiu_Lab8Context (DbContextOptions<Zakarias_Ovidiu_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Zakarias_Ovidiu_Lab8.Models.Book> Book { get; set; }
+    }
+}
