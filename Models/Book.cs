2968d63a138b88cfd82911832f3cab498f8c9834
Zakarias Ovidiu <zakarias.ovidiu@gmail.com>
2021-01-09 16:51:24 +0200

    Add project files.

---
+++ Models/Book.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Zakarias_Ovidiu_Lab8.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+
+        [Display(Name = "Book Title")]
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        [Column(TypeName = "decimal(6,2)")]
+        public decimal Price { get; set; }
+
+        [Display(Name = "Publishing Date")]
+        [DataType(DataType.Date)]
+        public DateTime PublishingDate { get; set; }
+
+        [ForeignKey("Publisher")]
+        public int PublisherID { get; set; }
+
+        public virtual Publisher Publisher { get; set; } // navigation property
+
+        public ICollection<BookCategory> BookCategories { get; set; }
+    }
+}
---
+++ Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Zakarias_Ovidiu_Lab8.Models
+{
+    public class Category
+    {
+        public int ID { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public ICollection<BookCategory> BookCategories { get; set; }
+    }
+}
